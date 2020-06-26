5e8ff518e092a98c6bf80ceee663837d8f1ac637
tdijkmans <tdijkmans@gmail.com>
2020-06-26 15:12:54 +0200

    Changed URL for Heroku

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+const heroku_api = "https://savoristas-back.herokuapp.com/";
+export const apiUrl = heroku_api || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;

02bd69e7db3f88029677da5b5c5d4187e8c35971
tdijkmans <tdijkmans@gmail.com>
2020-06-26 15:33:52 +0200

    For netlify

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,3 @@
-const heroku_api = "https://savoristas-back.herokuapp.com/";
-export const apiUrl = heroku_api || "http://localhost:4000";
+export const apiUrl = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;

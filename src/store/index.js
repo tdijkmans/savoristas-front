3b2c3573e9ae52e08a8423627d65311f20812c42
tdijkmans <tdijkmans@gmail.com>
2020-07-13 16:00:03 +0200

    Fix mobile

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,11 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-// const devTools = window.navigator.userAgent.includes("Chrome")
-//   ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   : compose;
+const devTools = window.navigator.userAgent.includes("Chrome")
+  ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  : compose;
 
-const enhancer = compose(applyMiddleware(ReduxThunk));
+const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 
 const store = createStore(reducer, enhancer);
 

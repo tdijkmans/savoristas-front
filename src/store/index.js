703d93d62f40c27e42c4df4603f3431b1684067b
tdijkmans <tdijkmans@gmail.com>
2020-07-13 16:12:42 +0200

    Devtool error

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools = window.navigator.userAgent.includes("Chrome")
-  ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  : compose;
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 

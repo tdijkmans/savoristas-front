c6eea33db4dfdeee6c27e490678518d823fc52b6
tdijkmans <tdijkmans@gmail.com>
2020-07-13 16:22:27 +0200

    Debug Redux Dev Tool for mobile display

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const devTools =
+  (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+  compose;
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 

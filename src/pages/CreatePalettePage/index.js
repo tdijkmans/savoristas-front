1108bceea355f94c1a3ac1cdd69590b9c7f9c778
tdijkmans <tdijkmans@gmail.com>
2020-08-12 11:01:35 +0200

    spellings store added for a.o. autocomplete

--- src/pages/CreatePalettePage/index.js
+++ src/pages/CreatePalettePage/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
-import CreatePaletteForm from "../../components/CreatePaletteForm";
+import CreatePaletteForm from "../../components/CreatePaletteForm"
 
-import { Box, Text } from "@chakra-ui/core";
+import { Box, Text, List } from "@chakra-ui/core"
+
+import { fetchSpellings } from "../../store/ingredientspellings/actions"
+import { selectIngredientSpellings } from "../../store/ingredientspellings/selectors"
 
 export default function CreatePalettePage() {
+  const dispatch = useDispatch()
+  const availableIngredients = useSelector(selectIngredientSpellings)
+
+  useEffect(() => {
+    dispatch(fetchSpellings())
+  }, [dispatch])
+
   return (
     <Box>
       <Box width="80%" marginLeft="auto" marginRight="auto">
@@ -12,7 +23,13 @@ export default function CreatePalettePage() {
           Post je eigen palet.
         </Text>
         <CreatePaletteForm />
+        LET'S USE BLEOW FOR INPUT:
+        <List>
+          {availableIngredients.map((i) => (
+            <li key={i.id}>{i.spelling}</li>
+          ))}
+        </List>
       </Box>
     </Box>
-  );
+  )
 }
---
+++ src/store/ingredientspellings/actions.js
@@ -0,0 +1,18 @@
+import { apiUrl } from "../../config/constants"
+import axios from "axios"
+import { selectUser } from "../user/selectors"
+
+export const FETCH_SPELLINGS_SUCCESS = "FETCH_SPELLINGS_SUCCESS"
+
+export const fetchSpellingsSuccess = (spellings) => ({
+  type: FETCH_SPELLINGS_SUCCESS,
+  payload: spellings
+})
+
+export const fetchSpellings = () => {
+  return async (dispatch, getState) => {
+    const response = await axios.get(`${apiUrl}/ingredientspellings`)
+
+    dispatch(fetchSpellingsSuccess(response.data.ingredientSpellings))
+  }
+}
